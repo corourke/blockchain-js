2f56fa26947be63de7b30c55fcfcaf4cb2ce31e5
Cameron <cameron.orourke@gmail.com>
2023-10-05 18:22:44 -0700

    Block class

---
+++ block.test.js
@@ -0,0 +1,29 @@
+const generateHash = require('./hashing')
+const Block = require('./block')
+
+const BLOCK0 = {
+  index: 0,
+  timestamp: Date.now(),
+  data: 'Genesis',
+  previousHash: '',
+}
+
+const block = new Block(BLOCK0)
+
+describe('Block', () => {
+  it('creates a Block object', () => {
+    expect(block).toBeInstanceOf(Block)
+  })
+  it('creates a block with the correct data', () => {
+    expect(block.getTimestamp()).toBe(BLOCK0.timestamp)
+    expect(block.getData()).toBe(BLOCK0.data)
+  })
+  it('has the correct hash', () => {
+    expect(block.getHash()).toEqual(
+      generateHash(
+        // assuming data is a string otherwise JSON.stringify
+        BLOCK0.index + BLOCK0.timestamp + BLOCK0.data + BLOCK0.previousHash
+      )
+    )
+  })
+})
